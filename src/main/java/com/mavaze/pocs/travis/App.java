7d8384c644f62a2c6e09325b942c028c408f5bd0
Mayuresh Vaze <mayur_vaze@yahoo.co.in>
2017-06-24 19:24:02 +0530

    first commit

---
+++ src/main/java/com/mavaze/pocs/travis/App.java
@@ -0,0 +1,13 @@
+package com.mavaze.pocs.travis;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
